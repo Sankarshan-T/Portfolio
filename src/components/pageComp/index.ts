e4838783a8bb19ed560fbb369860fae7e9863a62
CoolCream <mahasankarshant@gmail.com>
2026-06-12 05:30:23 +0530

    reorganized files

---
+++ src/components/pageComp/index.ts
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home.astro";
+export { default as About } from "./About.astro";
+export { default as Languages } from "./Languages.astro";
+export { default as Projects } from "./Projects.astro";
