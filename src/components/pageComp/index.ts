f28fd34b1ef3970eddc34d2b9dd3628f1fe1fd17
CoolCream <mahasankarshant@gmail.com>
2026-07-01 20:02:30 +0530

    im gonna use postgress for db tomorrrow. for now, its back to old state

--- src/components/pageComp/index.ts
+++ src/components/pageComp/index.ts
@@ -2,4 +2,3 @@ export { default as Home } from "./home.astro";
 export { default as About } from "./about.astro";
 export { default as Languages } from "./languages.astro";
 export { default as Projects } from "./projects.astro";
-export { default as Guestbook } from "./guestbook.astro";
