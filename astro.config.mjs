6320cb7a7373535e291a52e9096f5027ae5cafd1
CoolCream <mahasankarshant@gmail.com>
2026-07-02 19:04:56 +0530

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+    adapter: vercel(),
+
     vite: {
         plugins: [tailwindcss()],
         resolve: {
